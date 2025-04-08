7bffa2a349ab65d5e573bf80a2af7821a21947bc
Leo P <leo.projceski@education.nsw.gov.au>
2025-04-08 03:48:28 +0000

    started conditonals for menu page

--- my_work/main.py
+++ my_work/main.py
@@ -1,28 +1,42 @@
-# load menu with login, register, quit
+#register the user
+
+def menu():
+    print("1. Login")
+    print("2. Register")
+    print("3. Quit")
+    selection = input("Select an option: ")
+    if selection == 1:
+        login()
+    elif selection == 2:
+        register()
+    elif selection == 3:
+        quit()
+    else:
+        print("You have chosen an unsupported number.")
 
 def login():
     username = input("Enter your username: ")
     password = input("Enter your password: ")
-    file = open("plain_text.txt", "w")
-	file.write(username)
-	file.close()
 
-login()
+def register():
+    chosen_username = input("Choose a username: ")
+    chosen_password = input("Choose a password: ")
+
+def quit():
+    exit()
+
+menu()
+
 
 # ask user for their username and password
 
 ##username = input("Enter username: ")
 ##password = input("Enter password: ")
 
-# register user 
 
-##chosen_username = input("Choose a username: ")
-##chosen_password = input("Choose a password: ")
+
 
 # minimum 4 character password
 
-##if chosen_password > 4:
-    ##print("Password must be a minimum of 4 characters.")
 
-##else:
-    ##print("")
+    
